ef1f40f7474c9942a38dbea27dc5a8fd52f9b372
Johnu Celestine <johnuceles@gmail.com>
2017-08-21 22:43:51 -0400

    no message

--- src/test/java/com/celestine/johnu/reactor/ReactorApplicationTests.java
+++ src/test/java/com/celestine/johnu/reactor/ReactorApplicationTests.java
@@ -1,7 +1,5 @@
 package com.celestine.johnu.reactor;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,16 +11,6 @@ public class ReactorApplicationTests {
 
 	@Test
 	public void contextLoads() {
-	}
-	
-	@Test
-	public void testSuccess() {
-		assertEquals(1,1);
-	}
-	
-	@Test
-	public void testFailure() {
-		assertEquals(1,2);
 	}
 
 }
